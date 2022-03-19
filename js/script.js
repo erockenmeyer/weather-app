85c855d8deb53c8cb1a9f23dca5778409adaa5c9
erockenmeyer <erikrockenmeyer@gmail.com>
2022-03-18 20:39:38 -0500

    html setup

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var searchEl = document.querySelector("#city-search");
+var cityEl = document.querySelector("#city");
+var historyEl = document.querySelector("#history");
+var todayEl = document.querySelector("#today");
+var forecastEl = document.querySelector("#forecast");
